fd46a0f3332301e878eaa7c27e3036316143b5eb
Eduardo Alonso <edusantos@gmail.com>
2022-03-06 18:41:09 -0300

    user_model adjusted

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class UserModel {
   String? userName;
   String? userbio;
@@ -7,4 +9,29 @@ class UserModel {
     this.userbio,
     this.userUrl,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'userName': userName,
+      'userbio': userbio,
+      'userUrl': userUrl,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      userName: map['userName'],
+      userbio: map['userbio'],
+      userUrl: map['userUrl'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(json.decode(source));
+
+  @override
+  String toString() =>
+      'UserModel(userName: $userName, userbio: $userbio, userUrl: $userUrl)';
 }
